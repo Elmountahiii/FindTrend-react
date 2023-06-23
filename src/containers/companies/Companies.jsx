391c06496a49adaf95d294b1661e0b0742de2e8a
Youssef ElMountahi <yusuf.one@outlook.com>
2023-06-23 17:06:18 +0100

    implementing companies section

--- src/containers/companies/Companies.jsx
+++ src/containers/companies/Companies.jsx
@@ -1,7 +1,81 @@
 import "./companies.css";
 
+import brand1 from "../../assets/brand1.png";
+import brand2 from "../../assets/brand2.png";
+import brand3 from "../../assets/brand3.png";
+import brand4 from "../../assets/brand4.png";
+import brand5 from "../../assets/brand5.png";
+import brand6 from "../../assets/brand6.png";
+import brand7 from "../../assets/brand7.png";
+import brand8 from "../../assets/brand8.png";
+import brand9 from "../../assets/brand9.png";
+import brand10 from "../../assets/brand10.png";
+import brand11 from "../../assets/brand11.png";
+import brand12 from "../../assets/brand12.png";
+import brand13 from "../../assets/brand13.png";
+import brand14 from "../../assets/brand14.png";
+import brand15 from "../../assets/brand15.png";
+import brand16 from "../../assets/brand16.png";
+
 function Companies() {
-  return <div>Companies</div>;
+  return (
+    <div className="inside-container companies-container">
+      <div className="companies-headline">
+        <h1>Findtrend make +1000 Startup grow</h1>
+      </div>
+      <div className="companies-grid-container">
+        <div className="brand-container">
+          <img src={brand1} alt="" />
+        </div>
+
+        <div className="brand-container">
+          <img src={brand2} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand3} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand4} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand5} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand6} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand7} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand8} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand9} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand10} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand11} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand12} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand13} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand14} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand15} alt="" />
+        </div>
+        <div className="brand-container">
+          <img src={brand16} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Companies;
